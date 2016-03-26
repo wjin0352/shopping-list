0e37d36954978a93aa28af239569e57a531a4d25
won <wjin0352@gmail.com>
2016-03-25 21:07:41 -0400

    finished first make of site from wireframe

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('document').ready(function() {
+
+});
+
+
+
+// function generateTodoHTML(title, text) {
+//     return (
+//         '<div>' +
+//             '<h1>' + title + '</h2>' +
+//             '<p>' + text + '</p>' +
+//         '</div>'
+//     );
+// }
+
+// $('todo-list').append(
+//     generateTodoHTML('My Todo', 'do something')
+// );
