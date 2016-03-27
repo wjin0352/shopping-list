d0d6a59d66d312ed116394d9c2b90f24a30fdf77
won <wjin0352@gmail.com>
2016-03-27 03:54:09 -0400

    added done trash features

--- js/script.js
+++ js/script.js
@@ -1,28 +1,56 @@
-$('document').ready(function() {
-  // global var
+$(document).ready(function() {
+  // global var input box
   var inputPrompt = $('#add-items');
 
-  $('#add-items').keydown(function(event) {
+  $('#add-items').keydown( function(event) {
     if (event.keyCode == 13) {
-      createItem();
+      prependListItem();
       inputPrompt.val('');
       console.log(event)
     };
   });
 
+  $(document).on('click', '.trash', function() {
+    var item = $(this);
+    deleteListItem(item);
+    // console.log(item);
+  });
 
-   function createTodoHTML() {
-    var newItem = $('#add-items').val();
-    if (newItem != false) {
-      return ('<p><button class="trash btn btn-warning btn-lg">TRASH</button>' + newItem + '<button class="finished btn btn-success btn-lg">DONE!</button></p>');
-    }
+  $(document).on('click', '.finished', function() {
+    $(this).closest('p').addClass("fin");
+    console.log('here')
+  })
 
-    };
-
-  function createItem() {
-    $('.list-drop-down').prepend(createTodoHTML());
+  function createTodoHTML() {
+    var newItem = inputPrompt.val();
+      if (newItem != false) {
+        return ('<p><button class="trash btn btn-warning btn-lg">TRASH</button>' + newItem + '<button class="finished btn btn-success btn-lg">DONE!</button></p>');
+      };
   };
 
+  function prependListItem() {
+    $('.list-drop-down').prepend(createTodoHTML())
+        $('.list-drop-down p:first-child')
+        .css('opacity', "0")
+        .css("margin-top", "20px")
+        .animate(
+            { opacity: "1" },
+            { queue: true, duration: 'slow' }
+        )
+        .animate(
+            {marginTop: "0px"},
+            { queue: false, duration: 'slow' }
+        );;
+  };
+
+  function deleteListItem(item) {
+    $(item).closest('p').fadeOut(400);
+  };
+
+  function saveListItem(item) {
+    // $(item).
+
+  };
 });
 
 
