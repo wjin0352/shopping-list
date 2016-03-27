2d3ad5acb15938bdce3a005cf138d39ba3ad9145
won <wjin0352@gmail.com>
2016-03-27 02:17:03 -0400

    adding jquery features to add list items

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 $('document').ready(function() {
+  // global var
+  var inputPrompt = $('#add-items');
+
+  $('#add-items').keydown(function(event) {
+    if (event.keyCode == 13) {
+      createItem();
+      inputPrompt.val('');
+      console.log(event)
+    };
+  });
+
+
+   function createTodoHTML() {
+    var newItem = $('#add-items').val();
+    if (newItem != false) {
+      return ('<p><button class="trash btn btn-warning btn-lg">TRASH</button>' + newItem + '<button class="finished btn btn-success btn-lg">DONE!</button></p>');
+    }
+
+    };
+
+  function createItem() {
+    $('.list-drop-down').prepend(createTodoHTML());
+  };
 
 });
+
 
 
 
