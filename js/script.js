41219536b9e4eeed47058626e5ff37407708d9e9
won <wjin0352@gmail.com>
2016-03-28 01:21:17 -0400

    finished features for header input, memo

--- js/script.js
+++ js/script.js
@@ -2,24 +2,77 @@ $(document).ready(function() {
   // global var input box
   var inputPrompt = $('#add-items');
 
-  $('#add-items').keydown( function(event) {
-    if (event.keyCode == 13) {
-      prependListItem();
-      inputPrompt.val('');
-      console.log(event)
-    };
-  });
+  // Add Header Title feature
+    $('.input-title').keydown(function(event) {
+      // var keycode = (event.keyCode ? event.keyCode : event.which);
+      if (event.keyCode == 13) {
+        var title = $('.input-title').val();
+        createTitle(title);
+      };
+    });
 
-  $(document).on('click', '.trash', function() {
-    var item = $(this);
-    deleteListItem(item);
-    // console.log(item);
-  });
 
-  $(document).on('click', '.finished', function() {
-    $(this).closest('p').addClass("fin");
-    console.log('here')
-  })
+  // Edit Header Title feature
+    $(document).on('click', '.headerTitle', function(event) {
+      editTitle();
+    });
+
+  // Add list item feature
+    $('#add-items').keydown(function(event) {
+      if (event.keyCode == 13) {
+        prependListItem();
+
+        // sets the input box back to empty ''
+        inputPrompt.val('');
+      };
+    });
+
+  // Trash button feature
+    $(document).on('click', '.trash', function() {
+      var item = $(this);
+      deleteListItem(item);
+    });
+
+  // Done button feature
+  // Adds .fin class on finished list items, then removes .fin class
+  // on 2nd click
+    $(document).on('click', '.finished', function() {
+      if (!$(this).closest('p').hasClass('fin')) {
+        $(this).closest('p').addClass("fin");
+      }
+      else {
+        $(this).closest('p').removeClass('fin');
+      };
+    });
+
+    $(document).on('click', '.fin', function() {
+      $(this).closest('p').removeClass("fin");
+    });
+
+  // Creates a memo from input box
+    $('#description').keydown(function(event) {
+      var memo = $('#description').val();
+      if (event.keyCode == 13) {
+        createMemo(memo);
+      };
+    });
+
+  // Edits memo input box
+    $(document).on('click', '.memo', function() {
+      editMemo();
+    });
+
+  // // Memo save button
+  //   $(document).on('click', '.save', function() {
+  //     var memo = $('#description').val();
+
+  //   });
+
+  // // Memo delete button
+  //   $(document).on('click', '.delete', function() {
+  //     // $('.memo').val()='';
+  //     $('#description').show();
+  //   });
 
   function createTodoHTML() {
     var newItem = inputPrompt.val();
@@ -47,24 +100,25 @@ $(document).ready(function() {
     $(item).closest('p').fadeOut(400);
   };
 
-  function saveListItem(item) {
-    // $(item).
+  function createMemo(memo) {
+    var message = '<p class="memo">' + memo + '</p>';
+      $('input#description').hide();
+      $('.description').append(message);
+  };
 
+  function editMemo() {
+    $('.memo').hide();
+    $('#description').show().focus();
+  };
+
+  function createTitle(title) {
+    var mainTitle = '<p class="headerTitle">' + title + '</p>';
+    $('input.input-title').hide();
+    $('.header-title-section').append(mainTitle);
+  };
+
+  function editTitle() {
+    $('.headerTitle').hide();
+    $('.input-title').show().focus();
   };
 });
-
-
-
-
-// function generateTodoHTML(title, text) {
-//     return (
-//         '<div>' +
-//             '<h1>' + title + '</h2>' +
-//             '<p>' + text + '</p>' +
-//         '</div>'
-//     );
-// }
-
-// $('todo-list').append(
-//     generateTodoHTML('My Todo', 'do something')
-// );
